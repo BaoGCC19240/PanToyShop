2b29b0a58a2237187884b078ebb0f6206ae61662
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-10-27 22:39:50 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = mysqli_connect('localhost','root','','pantoyshop')
-    or die("Can not connect database".mysqli_connect_error());
+    $conn =pg_connect("postgres://perrguastfkmhh:6f5304c59a0e10aadff5c6b1105a7227c24153d357b861a46a71049c833722d9@ec2-44-195-240-222.compute-1.amazonaws.com:5432/d7ht733vfk2d0p");
+
 ?>
