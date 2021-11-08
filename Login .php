766c891583817e1e979b7b20f7cee54efdbc6d82
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-08 16:59:31 +0700

    Update Login .php

--- Login .php
+++ Login .php
@@ -21,7 +21,7 @@ if(isset($_POST['btnLogin'])){
         include_once("connection.php");
         $us=pg_escape_string($us);
         $pass=md5("$pa");
-        $sq = "Select username, password, state from customer where Username='$us' and Password='$pass'";
+        $sq = "Select username, password, state from customer where username='$us' and password='$pass'";
         $res= pg_query($conn,$sq);
         $check = pg_num_rows($res);
         $row = pg_fetch_row($res);
