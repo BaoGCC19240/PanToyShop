806ecb35de359e6a48483ae7cebf9ac1351b5ae6
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-08 18:34:34 +0700

    Update Update_customer.php

--- Update_customer.php
+++ Update_customer.php
@@ -71,14 +71,14 @@ if(isset($_POST['btnUpdate'])){
 						    
                             <label for="lblTenDangNhap" class="col-sm-2 control-label">Username(*):  </label>
 							<div class="col-sm-10">
-							      <label class="form-control" style="font-weight:400"><?php echo $us;  ?></label>
+                                <input type="text"  id="txtAddress" value="<?php  echo $us;  ?>" class="form-control" />
 							</div>
                      </div>
                            
                          <div class="form-group">   
                             <label for="lblEmail" class="col-sm-2 control-label">Email(*):  </label>
 							<div class="col-sm-10">
-							       <label class="form-control" style="font-weight:400"><?php echo $email;  ?></label>
+                                <input type="text" id="txtAddress" value="<?php  echo $email;  ?>" class="form-control"/>
 							</div>
                           </div>  
                           
