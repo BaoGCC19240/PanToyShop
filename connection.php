5b72bd15b822d8b735b3012aa93947459c714d40
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-08 17:13:55 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,4 +2,5 @@
     $Connect = pg_connect("postgres://thrrzgwslydqii:4db6d5758b42874a2f1342dd754b3441d3fa00255415a7d7eb35c3d0a685e201@ec2-54-226-56-198.compute-1.amazonaws.com:5432/d2hgkknu6p6pgi");
     if (!$Connect) {
         die("Connection failed");
+    }
 ?>
