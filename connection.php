6ecce0b8411228633fe4839258f4c62e4e8a0bcd
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-08 17:12:50 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,5 @@
 <?php
-$conn = pg_connect("host=ec2-54-226-56-198.compute-1.amazonaws.com port=5432 dbname=d2hgkknu6p6pgi user=thrrzgwslydqii password=4db6d5758b42874a2f1342dd754b3441d3fa00255415a7d7eb35c3d0a685e201");
+    $Connect = pg_connect("postgres://thrrzgwslydqii:4db6d5758b42874a2f1342dd754b3441d3fa00255415a7d7eb35c3d0a685e201@ec2-54-226-56-198.compute-1.amazonaws.com:5432/d2hgkknu6p6pgi");
+    if (!$Connect) {
+        die("Connection failed");
 ?>
