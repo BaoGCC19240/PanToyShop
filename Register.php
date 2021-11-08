9df4723fbdbaa12ac5575795442b5f331ba31786
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-08 15:04:50 +0700

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="style.css"/>
 <meta charset="utf-8" />
-<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="css/responsive.css">
 <script src="js/jquery-3.2.0.min.js"></script>
 <script src="js/jquery.dataTables.min.js"></script>
@@ -20,9 +20,7 @@
      {
          $sex=$_POST['grpRender'];
      }
-     $date=$_POST['slDate'];
-     $month=$_POST['slMonth'];
-     $year=$_POST['slYear'];
+
      $err="";
      if($us==""||$pass1==""||$pass2==""||$fullname==""||$email=="" ||$address==""||!isset($sex))
      {
@@ -34,20 +32,17 @@
      if($pass1!=$pass2){
         $err.="<li>Password and confirm password are not the same</li>";
     }
-    if($_POST['slYear']=="0"){
-        $err.="<li>Choose Year of Birth, please</li>";
-    }
     if($err!=""){
         echo $err;
     }
     else{
         include_once("connection.php");
         $pass=md5($pass1);
-        $sq="select * from customer where Username='$us' or email='$email'";
-        $res=pg_query($sq);
+        $sq="select * from customer where Username='$us' or Cus_Email='$email'";
+        $res=pg_query($conn,$sq);
         if(pg_num_rows($res)==0){
-            pg_query("Insert into customer (Username, Password, CustName,gender,Address,telephone,email, CusDate, CusMonth, CusYear,SSN,ActiveCode, state) values('$us','$pass','$fullname','$sex','$address','$tel','$email','$date','$month','$year','','',0)")
-            or die(pg_error());
+            pg_query($conn,"Insert into customer (Username, Password, CustName,Gender,Cus_Address,Cus_Tel,Cus_Email, State) values('$us','$pass','$fullname','$sex','$address','$tel','$email',0)")
+            or die(pg_error($conn));
             echo"You have registered successfully";
         }
         else{
@@ -67,6 +62,13 @@
 							</div>
                       </div>  
                       
+                      <div class="form-group">                               
+                            <label for="lblFullName" class="col-sm-2 control-label bre">Full name(*):  </label>
+							<div class="col-sm-10">
+							      <input type="text" name="txtFullname" id="txtFullname" value="" class="form-control bre" placeholder="Enter Fullname"/>
+							</div>
+                       </div> 
+
                        <div class="form-group">   
                             <label for="" class="col-sm-2 control-label bre">Password(*):  </label>
 							<div class="col-sm-10">
@@ -81,12 +83,7 @@
 							</div>
                        </div>     
                        
-                       <div class="form-group">                               
-                            <label for="lblFullName" class="col-sm-2 control-label bre">Full name(*):  </label>
-							<div class="col-sm-10">
-							      <input type="text" name="txtFullname" id="txtFullname" value="" class="form-control bre" placeholder="Enter Fullname"/>
-							</div>
-                       </div> 
+                       
                        
                        <div class="form-group">      
                             <label for="lblEmail" class="col-sm-2 control-label bre">Email(*):  </label>
@@ -94,6 +91,13 @@
 							      <input type="text" name="txtEmail" id="txtEmail" value="" class="form-control bre" placeholder="Email"/>
 							</div>
                        </div>  
+
+                       <div class="form-group">  
+                            <label for="lblDienThoai" class="col-sm-2 control-label bre">Telephone(*):  </label>
+							<div class="col-sm-10">
+							      <input type="text" name="txtTel" id="txtTel" value="" class="form-control bre" placeholder="Telephone" />
+							</div>
+                         </div> 
                        
                         <div class="form-group">   
                              <label for="lblDiaChi" class="col-sm-2 control-label bre">Address(*):  </label>
@@ -102,12 +106,7 @@
 							</div>
                         </div>  
                         
-                         <div class="form-group">  
-                            <label for="lblDienThoai" class="col-sm-2 control-label bre">Telephone(*):  </label>
-							<div class="col-sm-10">
-							      <input type="text" name="txtTel" id="txtTel" value="" class="form-control bre" placeholder="Telephone" />
-							</div>
-                         </div> 
+                         
                          
                           <div class="form-group">  
                             <label for="lblGioiTinh" class="col-sm-2 control-label bre">Gender(*):  </label>
@@ -121,45 +120,7 @@
 							</div>
                           </div> 
                           
-                          <div class="form-group bre"> 
-                            <label for="lblNgaySinh" class="col-sm-2 control-label">Date of Birth(*):  </label>
-                            <div class="col-sm-10 input-group bre">
-                                <span class="input-group-btn">
-                                  <select name="slDate" id="slDate" class="form-control" >
-                						<option value="0">Choose Date</option>
-										<?php
-                                            for($i=1;$i<=31;$i++)
-                                             {                                                
-                                                 echo "<option value='".$i."'>".$i."</option>";
-                                             }
-                                        ?>
-                				 </select>
-                                </span>
-                                <span class="input-group-btn">
-                                  <select name="slMonth" id="slMonth" class="form-control">
-                					<option value="0">Choose Month</option>
-									<?php
-                                        for($i=1;$i<=12;$i++)
-                                         {
-                                             echo "<option value='".$i."'>".$i."</option>";
-                                         }
                           
-                                    ?>
-                				</select>
-                                </span>
-                                <span class="input-group-btn">
-                                  <select name="slYear" id="slYear" class="form-control">
-                                    <option value="0">Choose Year</option>
-                                    <?php
-                                        for($i=1970;$i<=2020;$i++)
-                                         {
-                                             echo "<option value='".$i."'>".$i."</option>";
-                                         }
-                                    ?>
-                                </select>
-                                </span>
-                           </div>
-                      </div>	
 					<div class="form-group">
 						<div class="col-sm-10">
 						      <input type="submit"  class="btn btn-primary" name="btnRegister" id="btnRegister" value="Register"/>
