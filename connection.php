07665bd36f1cc5956d7fca1be3d8c20a453c1f1b
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-08 15:20:11 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,3 @@
 <?php
-    $conn =pg_connect("postgres://thrrzgwslydqii:4db6d5758b42874a2f1342dd754b3441d3fa00255415a7d7eb35c3d0a685e201@ec2-54-226-56-198.compute-1.amazonaws.com:5432/d2hgkknu6p6pgi")
-    or die("Can not connect database".pg_connect_error());
+$conn =pg_connect("postgres://thrrzgwslydqii:4db6d5758b42874a2f1342dd754b3441d3fa00255415a7d7eb35c3d0a685e201@ec2-54-226-56-198.compute-1.amazonaws.com:5432/d2hgkknu6p6pgi");
 ?>
