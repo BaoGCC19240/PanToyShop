da63ffe72a5ad5abb4b257dec6cca8634a02a466
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-09 16:40:08 +0700

    Update Product_Management.php

--- Product_Management.php
+++ Product_Management.php
@@ -55,7 +55,7 @@
                 $result=pg_query($conn,"select Pro_ID, Pro_Name,Pro_Price,Pro_qty,Pro_image,Cat_Name, shop_name
                 From product a, category b , shop c
                 Where a.Cat_ID =b.Cat_ID and a.shop_id = c.shop_id");
-
+                var_dump($result);
                 while($row=pg_fetch_array($result)){
                 ?>
 			<tr>
