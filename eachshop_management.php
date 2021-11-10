c79768d3830361d27f40d218ef689cad8267b542
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-10 23:52:34 +0700

    Update eachshop_management.php

--- eachshop_management.php
+++ eachshop_management.php
@@ -48,7 +48,9 @@
             $result =pg_query($conn,"Select shop_name, pro_name, pro_qty, or_qty  from shop sh, product pro, orderdetail ord where ord.pro_id= pro.pro_id and sh.shop_id=pro.shop_id and sh.shop_id =$idshop");
             while($row=pg_fetch_array($result))
             {
-                $pro_num = !empty($row['or_qty']) ? $row['or_qty']:0;
+                if($row['or_qty']==""){
+                    $row['or_qty']=0;
+                }
 
                 ?>
 			<tr>
