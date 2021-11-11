4afe3b3ee97c9a0fa02c35cd410b027ec7de5439
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-11 13:17:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PanSop</title>
+    <title>ATN Shop</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
