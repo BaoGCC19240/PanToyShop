20470892fedd7bc28331f06bb017006e543f22a7
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-11 23:10:10 +0700

    Update Update_Category.php

--- Update_Category.php
+++ Update_Category.php
@@ -11,9 +11,8 @@
 		$result = pg_query($conn,"Select * from category Where Cat_ID='$id'");
 		$row=pg_fetch_array($result);
 		$cat_id=htmlspecialchars(pg_escape_string($row['cat_id']));
-		var_dump($row['cat_name']);
-		$cat_name=htmlspecialchars(pg_escape_string($row['cat_name']));
-		$cat_des=htmlspecialchars(pg_escape_string($row['cat_desc']));
+		$cat_name=htmlspecialchars(pg_escape_string($row["cat_name"]));
+		$cat_des=htmlspecialchars(pg_escape_string($row["cat_desc"]));
 
    if(isset($_POST["btnUpdate"]))
    {
