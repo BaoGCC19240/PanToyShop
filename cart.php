96e43d94dde1dcc7889e6b3db23069e5eb1c85c1
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-11 10:53:37 +0700

    Update cart.php

--- cart.php
+++ cart.php
@@ -57,7 +57,7 @@
                                 $sq ="insert into orderdetail (pro_id, or_qty, or_amount,or_status,or_date, username) values ('$key','$val','$sum','unconfimred','$today','$us')";
                                 pg_query($conn,$sq);
                                 $qtyremain = pg_query($conn,"select pro_qty from product where pro_id = '$us'");
-                                pg_query($conn, "Update product set pro_qty = $qtyremain - $val where pro_id = '$us'");
+                                pg_query($conn, "Update product set pro_qty = $qtyremain - $val where pro_id = '$key'");
                                 unset($_SESSION['cart']);
 
                                 echo "<h3>Order Success</h3>";
