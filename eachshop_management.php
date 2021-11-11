80e8c08d071b92178c63430122b620745f6a3117
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-11 15:32:56 +0700

    Update eachshop_management.php

--- eachshop_management.php
+++ eachshop_management.php
@@ -49,9 +49,8 @@
                 $proid= $row['pro_id'];
                 var_dump($proid);
 
-                $sq=pg_query($conn,"select sum(or_qty) as "sum" from orderdetail where pro_id='$proid'");
+                $sq=pg_query($conn,"select sum(or_qty) from orderdetail where pro_id='$proid'");
                 $num = pg_fetch_array($sq);
-                var_dump($num['sum']);
 
                 ?>
 			<tr>
