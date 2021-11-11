228ce42bb69f369c438d4281f75621b0a9599927
BaoGCC19240 <baopcgcc19240@fpt.edu.vn>
2021-11-11 22:04:51 +0700

    a

--- Category_Management.php
+++ Category_Management.php
@@ -87,7 +87,7 @@
         </div><!--Nút chức nang-->
  </form>
             </div>
-<script type="text/javascript" src="js/bootstrap.js"></script>
+<script src="js/bootstrap.js"></script>
 <script type="text/javascript" src="js/npm.js"></script>
 <script src="js/jquery-2.1.0.min.js"></script>
 
